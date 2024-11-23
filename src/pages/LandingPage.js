cd52cec4413e9658012bdc35d3ee2a16758bae27
ANUJ KUMAR SINGH <151390316+MUFFANUJ@users.noreply.github.com>
2024-11-23 20:34:52 +0530

    Update LandingPage.js

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -13,6 +13,9 @@ import live from "../assets/Live.png";
 // import TestimonialCard from "../assets/TestimonialCard.svg"
 import { GoAlertFill } from "react-icons/go";
 import { useNavigate } from "react-router-dom";
+import group1 from "../assets/Group1.png";
+import group2 from "../assets/Group2.png";
+import group3 from "../assets/Group3.png";
 
 gsap.registerPlugin(ScrollTrigger);
 export default function LandingPage() {
@@ -41,23 +44,22 @@ export default function LandingPage() {
   ];
 
   useEffect(() => {
-    if (localStorage.getItem('loggedIn') === "true"){
-      navigate("/dashboard")
-      return
+    if (localStorage.getItem("loggedIn") === "true") {
+      navigate("/dashboard");
+      return;
     }
-    
+
     const images = imagesRef.current;
     // const texts = textRef.current;
-    gsap.set(images, { opacity: 0, position: "absolute", top: 0, left: 0 });
+    gsap.set(images, { opacity: 0, position: "absolute", top: "5%", left: "35%" });
     gsap.set(images[0], { opacity: 1 });
     // gsap.set(texts, { opacity: 0 });
     // gsap.set(texts[0], { opacity: 1 });
 
-
     images.forEach((image, index) => {
-      const scrollStart = 400 * index; 
-      const scrollEnd = 400 * (index + 1); 
-      
+      const scrollStart = 400 * index;
+      const scrollEnd = 400 * (index + 1);
+
       ScrollTrigger.create({
         trigger: containerRef.current,
         start: `${scrollStart}vh top`,
@@ -76,40 +78,45 @@ export default function LandingPage() {
         },
       });
     });
-    
-    
-    const backgroundColors = ["#ffffff", "radial-gradient(50% 50% at 50% 50%, #4602D9 0%, #111111 100%)", "radial-gradient(50% 50% at 50% 50%, #FF3A44BF 0%, #111111 100%)"]; 
+
+    const backgroundColors = [
+      "#ffffff",
+      "radial-gradient(50% 50% at 50% 50%, #4602D9 0%, #111111 100%)",
+      "radial-gradient(50% 50% at 50% 50%, #FF3A44BF 0%, #111111 100%)",
+    ];
     const container = containerRef.current;
     backgroundColors.forEach((color, index) => {
-      const scrollStart = 400 * index; 
-      const scrollEnd = 400 * (index + 1); 
-  
+      const scrollStart = 400 * index;
+      const scrollEnd = 400 * (index + 1);
+
       ScrollTrigger.create({
         trigger: container,
-        start: `${scrollStart}vh top`, 
+        start: `${scrollStart}vh top`,
         end: `${scrollEnd}vh top`,
-        onEnter: () =>{ 
-          gsap.to(container, { background: color, duration: 0.5 })},
-        onEnterBack: () =>{
-          gsap.to(container, { background: color, duration: 0.5 })},
+        onEnter: () => {
+          gsap.to(container, { background: color, duration: 0.5 });
+        },
+        onEnterBack: () => {
+          gsap.to(container, { background: color, duration: 0.5 });
+        },
       });
     });
 
     // backgroundColors.forEach((color, index) => {
-    //   const scrollStart = 800 * index; 
-    //   const scrollEnd = 800 * (index + 1); 
-  
+    //   const scrollStart = 800 * index;
+    //   const scrollEnd = 800 * (index + 1);
+
     //   ScrollTrigger.create({
     //     trigger: container,
-    //     start: `${scrollStart}vh top`, 
+    //     start: `${scrollStart}vh top`,
     //     end: `${scrollEnd}vh top`,
-    //     onEnter: () =>{ 
+    //     onEnter: () =>{
     //       setActiveButtonIndex(prev => prev+1)},
     //     onEnterBack: () =>{
     //       setActiveButtonIndex(prev => prev-1)},
     //   });
     // });
-  
+
     // const sections = imagesRef.current;
     // texts.forEach((image, index) => {
     //   const scrollStart = 15* index; // Start the trigger at 150vh increments
@@ -132,12 +139,12 @@ export default function LandingPage() {
   }, []);
 
   const handleButtonClick = (index) => {
-    setActiveButtonIndex(index); 
-    scrollToImage(index); 
+    setActiveButtonIndex(index);
+    scrollToImage(index);
   };
 
   const scrollToImage = (index) => {
-    const scrollPosition = 400 * index; 
+    const scrollPosition = 400 * index;
     window.scrollTo({
       top: scrollPosition,
       behavior: "smooth",
@@ -145,7 +152,6 @@ export default function LandingPage() {
   };
 
   return (
-
     <div className={styles.landingBody} style={{ height: "350vh" }}>
       <div>
         <div className={styles.header}>
@@ -163,8 +169,8 @@ export default function LandingPage() {
         </div> */}
 
           <div className={styles.downloadBtns}>
-            <img src={appStore} alt="app store logo"/>
-            <img src={googlePlay}  alt="google playstore logo"/>
+            <img src={appStore} alt="app store logo" />
+            <img src={googlePlay} alt="google playstore logo" />
           </div>
         </div>
       </div>
@@ -176,40 +182,40 @@ export default function LandingPage() {
           backgroundColor: "white",
           color: "black",
           top: "0px",
+          height: "100vh",
         }}
       >
         <div className={styles.holdingPhone}>
-          <img className={styles.mainBackgroundImage} src={holdingPhone} alt="a hand holding a phone"/>
-
-          <div>
+          <div >
             <div className={styles.screenImage}>
-              <img
-                ref={(el) => (imagesRef.current[0] = el)}
-                src={screen1}
-                alt="First Screen"
-                className={styles.changingImages}
-              />
-              <img
-                ref={(el) => (imagesRef.current[1] = el)}
-                src={screen2}
-                alt="Second Screen"
-                className={styles.changingImages}
-              />
-              <div className="container">
-  <img
-    ref={(el) => (imagesRef.current[2] = el)}
-    src={screen3}
-    alt="Third Screen"
-    className={styles.changingImages}
-  />
-  {/* <img src={TestimonialCard} alt="Testimonial" className={styles.TestimonialCard} /> */}
-</div>
-
+              <div>
+                <img
+                  className={styles.mainBackgroundImage}
+                  src={group1}
+                  alt="a hand holding a phone"
+                  ref={(el) => (imagesRef.current[0] = el)}
+                />
+                <img
+                  className={styles.mainBackgroundImage}
+                  src={group2}
+                  alt="a hand holding a phone"
+                  ref={(el) => (imagesRef.current[1] = el)}
+                />
+                <img
+                  className={styles.mainBackgroundImage}
+                  src={group3}
+                  alt="a hand holding a phone"
+                  ref={(el) => (imagesRef.current[2] = el)}
+                />
+              </div>
+              
               <div className={styles.navigationBtns}>
                 <button
                   ref={(el) => (buttonsRef.current[0] = el)}
                   onClick={() => handleButtonClick(1.5)}
-                  className={`${styles.navigationBtnsButton} ${activeButtonIndex === 1.5 ? styles.activeButton : ""}`}
+                  className={`${styles.navigationBtnsButton} ${
+                    activeButtonIndex === 1.5 ? styles.activeButton : ""
+                  }`}
                 >
                   Currency Converter
                   <span>&#8377;</span>
@@ -217,17 +223,21 @@ export default function LandingPage() {
                 <button
                   ref={(el) => (buttonsRef.current[1] = el)}
                   onClick={() => handleButtonClick(2.5)}
-                  className={`${styles.navigationBtnsButton} ${activeButtonIndex === 2.5 ? styles.activeButton : ""}`}
+                  className={`${styles.navigationBtnsButton} ${
+                    activeButtonIndex === 2.5 ? styles.activeButton : ""
+                  }`}
                 >
                   Live rates
                   <span>
-                    <img style={{ width: "15px" }} src={live} alt="live logo"/>
+                    <img style={{ width: "15px" }} src={live} alt="live logo" />
                   </span>
                 </button>
                 <button
                   ref={(el) => (buttonsRef.current[2] = el)}
                   onClick={() => handleButtonClick(3.5)}
-                  className={`${styles.navigationBtnsButton} ${activeButtonIndex === 3.5 ? styles.activeButton : ""}`}
+                  className={`${styles.navigationBtnsButton} ${
+                    activeButtonIndex === 3.5 ? styles.activeButton : ""
+                  }`}
                 >
                   Set rate alert
                   <span>
